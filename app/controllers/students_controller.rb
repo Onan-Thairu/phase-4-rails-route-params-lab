edb2485c63e62aa4075e7122208eb84999539adb
OnanThairu <thairuonan@gmail.com>
2022-09-29 10:09:34 +0300

    Bonus: Add query params.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,9 +2,10 @@ class StudentsController < ApplicationController
 
   def index
     if params[:name]
-      students = Student.where("first_name = ?", params[:name]).or(Student.where("last_name = ?", params[:name]))
+      # students = Student.where("first_name = ?", params[:name]).or(Student.where("last_name = ?", params[:name]))
+      students = Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
       render json: students
-    elsif
+    else
       students = Student.all
       render json: students
     end
